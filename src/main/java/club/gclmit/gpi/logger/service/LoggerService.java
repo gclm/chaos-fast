bf12448117bca6066f73cc67d3bf1d2c54d7e521
gclm <1719982754@qq.com>
2020-04-14 18:02:14 +0800

    fix(修改发布gpi版本):

---
+++ src/main/java/club/gclmit/gpi/logger/service/LoggerService.java
@@ -0,0 +1,16 @@
+package club.gclmit.gpi.logger.service;
+
+import club.gclmit.gpi.logger.pojo.HttpTrace;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ *  服务类
+ * </p>
+ *
+ * @author 孤城落寞
+ * @since 2019-12-17
+ */
+public interface LoggerService extends IService<HttpTrace> {
+
+}
